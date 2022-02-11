6179121dd7019438b84ba73eb0cfc589f5df8f34
Michael Gutierrez <michaelgutierrezj@gmail.com>
2022-02-11 09:54:08 -0600

    commit for Task 1 Header function completion, returns correct markup

--- src/components/header.js
+++ src/components/header.js
@@ -11,6 +11,23 @@ const Header = (title, date, temp) => {
   //    <span class="temp">{ temp }</span>
   //  </div>
   //
+  const headerDiv = document.createElement("div");
+  const dateSpan = document.createElement("span");
+  const h1 = document.createElement("h1");
+  const tempSpan = document.createElement("span");
+
+  dateSpan.textContent = date;
+  h1.textContent = title;
+  tempSpan.textContent = temp;
+  headerDiv.classList.add("header");
+  dateSpan.classList.add("date");
+  tempSpan.classList.add("temp");
+
+  headerDiv.appendChild(dateSpan);
+  headerDiv.appendChild(h1);
+  headerDiv.appendChild(tempSpan);
+
+  return headerDiv;
 }
 
 const headerAppender = (selector) => {
